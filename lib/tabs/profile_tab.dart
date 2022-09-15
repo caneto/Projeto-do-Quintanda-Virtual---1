6e7df0e242405e500a9eb679843f6f622b484269
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-09-15 18:22:39 -0300

    Atualização

--- lib/tabs/profile_tab.dart
+++ lib/tabs/profile_tab.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quitandavirtual/components/custom_text_field.dart';
 import 'package:quitandavirtual/config/app_data.dart' as appData;
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({Key? key}) : super(key: key);
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,19 +55,90 @@ class ProfileTab extends StatelessWidget {
             height: 50,
             child: OutlinedButton(
               style: OutlinedButton.styleFrom(
-                side: const BorderSide(
-                  color: Colors.green
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )
-              ),
-              onPressed: () {},
+                  side: const BorderSide(color: Colors.green),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+              onPressed: () {
+                updatePassword();
+              },
               child: const Text("Atualizar senha"),
             ),
           )
         ],
       ),
     );
+  }
+
+  Future<bool?> updatePassword() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          "Atualização de senha",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const CustomTextField(
+                        isSecret: true,
+                        icon: Icons.lock,
+                        label: 'Senha atual',
+                      ),
+                      const CustomTextField(
+                        isSecret: true,
+                        icon: Icons.lock_outline,
+                        label: 'Nova senha',
+                      ),
+                      const CustomTextField(
+                        isSecret: true,
+                        icon: Icons.lock_outline,
+                        label: 'Confirmar nova senha',
+                      ),
+                      SizedBox(
+                        height: 45,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          )),
+                          onPressed: () {},
+                          child: const Text("Atualizar"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
