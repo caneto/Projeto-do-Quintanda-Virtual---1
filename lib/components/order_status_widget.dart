95b5f9958cb41835344453c75875fa1b0a17989f
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-09-19 09:32:47 -0300

    Atualização

--- lib/components/order_status_widget.dart
+++ lib/components/order_status_widget.dart
@@ -1,27 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:quitandavirtual/config/custom_colors.dart';
 
 class OrderStatusWidget extends StatelessWidget {
   final String status;
   final bool isOverdue;
 
-  const OrderStatusWidget({Key? key, required this.status, required this.isOverdue}) : super(key: key);
+  final Map<String, int> allStatus = <String, int>{
+    'peding_payment':0,
+    'refunded':1,
+    'paid':2,
+    'preparing_purchase':3,
+    'shipping':4,
+    'delivared':5,
+  };
+
+  int get currentStatus => allStatus[status]!;
+
+  OrderStatusWidget(
+      {Key? key, required this.status, required this.isOverdue})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const _StatusDot(
+          isActive: true,
+          title: 'Pedido Confirmado',
+        ),
+        _CustomDivider(),
+        if(currentStatus == 1)  ...[
+          const _StatusDot(
+            isActive: true,
+            title: 'Pix estornado',
+            backgroundColor: Colors.deepOrangeAccent,
+          ),
+        ] else if (isOverdue) ...[
+          const _StatusDot(
+            isActive: true,
+            title: 'Pagamento Pix vencido',
+            backgroundColor: Colors.redAccent,
+          ),
+        ]
+
 
       ],
     );
   }
-
 }
 
-class _StatusDot extends StatelessWidget {
-  const _StatusDot({Key? key}) : super(key: key);
+class _CustomDivider extends StatelessWidget {
+  const _CustomDivider({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 3,
+      ),
+      height: 10,
+      width: 2,
+      color: Colors.grey.shade300,
+    );
+  }
+}
+
+class _StatusDot extends StatelessWidget {
+  final bool isActive;
+  final String title;
+  final Color? backgroundColor;
+
+  const _StatusDot({Key? key, required this.isActive, required this.title, this.backgroundColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          height: 20,
+          width: 20,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: CustomColors.customSwatchColor),
+            color:
+                isActive ? backgroundColor ?? CustomColors.customSwatchColor : Colors.transparent,
+          ),
+          child: isActive
+              ? const Icon(
+                  Icons.check,
+                  size: 13,
+                  color: Colors.white,
+                )
+              : const SizedBox.shrink(),
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Expanded(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
